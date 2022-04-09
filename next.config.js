5a1d4d462151edb50e233d02b4c37d19057f0afc
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-09 19:00:24 +0900

    Update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/udagawa-shinra-project",
+  async rewrites() {
+    return [
+      {
+        source: "/_next",
+        destination: "/",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
