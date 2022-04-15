e23f644526a6eadf2fc821c966fa79ddb9fd8538
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-15 14:02:05 +0900

    Fix style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,32 +6,6 @@ import { jaJP } from "@mui/material/locale";
 
 const theme = createTheme(
   {
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
-        h1, h2, p {
-          margin: 1rem 0 0 0;
-        }
-        h1:first-of-type, h2:first-of-type, p:first-of-type {
-          margin: 0;
-        }
-        h1 {
-          font-size: 2rem;
-        }
-        h2 {
-          font-size: 1.5rem;
-        }
-        p {
-          font-size: 1rem;
-        }
-        iframe {
-          aspect-ratio: 16 / 9;
-          border: none;
-          width: 100%;
-        }
-      `,
-      },
-    },
     typography: {
       fontFamily: [
         "Noto Sans JP",
