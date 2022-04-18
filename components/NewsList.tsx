04ee85d5a9fa67e5eabf281d91ed69f37e7913e1
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-18 17:38:45 +0900

    Add news

---
+++ components/NewsList.tsx
@@ -0,0 +1,17 @@
+import { List, ListItem, ListItemText, Divider } from "@mui/material";
+
+interface Props {
+  items: JSX.Element[];
+}
+
+export const NewsList = ({ items }: Props) => {
+  const newsItems = items.map((item, index) => (
+    <>
+      <ListItem key={index} disablePadding>
+        <ListItemText primary={item} />
+      </ListItem>
+      <Divider component="li" />
+    </>
+  ));
+  return <List>{newsItems}</List>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,64 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { NewsList } from "../components/NewsList";
 import { Box, Paper, CardContent, CardMedia, Card } from "@mui/material";
+
+const newsItems = [
+  <>
+    言語処理学会第28回年次大会(NLP2022)で森羅プロジェクトの
+    <a href="https://drive.google.com/file/d/1fHXpMsTc1AMu19aDGzIHz6GLKJbRJMae/view?usp=sharing">
+      発表
+    </a>
+    を行い、委員特別賞を
+    <a href="http://shinra-project.info/wp-content/uploads/sites/8/2022/03/委員特別賞.png">
+      受賞
+    </a>
+    しました。(2022/3/18)
+  </>,
+  <>
+    <span>
+      森羅プロジェクトが英国の雑誌Impactの
+      <a href="http://shinra-project.info/wp-content/uploads/sites/8/2022/02/Impact-publication.pdf">
+        記事
+      </a>
+      として取り上げられました。どうぞご覧ください。(2022/2/24)
+    </span>
+  </>,
+  <>
+    森羅2021の最終報告会(12月20日)にご参加いただき、ありがとうございました。資料を
+    <a href="http://shinra-project.info/shinra2021-final-report-meeting/">
+      最終報告会ページ
+    </a>
+    に掲載していますので、どうぞご覧ください。(2021/12/24)
+  </>,
+  <>
+    LinkJPタスクの
+    <a href="https://drive.google.com/file/d/1Lqy480YwG8SbTWz56I_w6IKEw421n5IM/view?usp=sharing">
+      テストデータ正解
+    </a>
+    を公開しました。(2021/11/15)
+  </>,
+  <>
+    MLタスクの実行結果の提出〆切を11月15日に延長しました。ぜひタスク参加をご検討下さい。(2021/10/4)
+  </>,
+  <>
+    LinkJPタスクの
+    <a href="https://drive.google.com/file/d/1iEciat50vSaGJ9d9FQz20k8eRt2mMGN7/view?usp=sharing">
+      テストデータ
+    </a>
+    を公開しました。
+    <a href="http://shinra-project.info/shinra2021linkjp/result_submission/">
+      実行結果提出
+    </a>
+    の締切は9月30日です。(2021/9/10)
+  </>,
+  <>
+    森羅プロジェクトの紹介ビデオを公開しました（[
+    <a href="https://www.youtube.com/watch?v=lCYj3x6pu1w">日本語版</a>][
+    <a href="https://www.youtube.com/watch?v=yQ2xeq7rbeU">英語版</a>
+    ])。（2021/6/30）
+  </>,
+];
 
 const Home: NextPage = () => {
   return (
@@ -60,8 +118,7 @@ const Home: NextPage = () => {
       >
         <CardContent>
           <h2>新着情報</h2>
-          <p>概</p>
-          <p>要</p>
+          <NewsList items={newsItems} />
         </CardContent>
       </Card>
       <Card
