cfeb2834513c23db53ecdc7533a1b70be46c3b88
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-19 15:43:56 +0900

    Fix type

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,4 @@
-export { CodeBlock, Props as Code } from "./CodeBlock";
+export { CodeBlock } from "./CodeBlock";
+export type { Props as Code } from "./CodeBlock";
 export { NewsList } from "./NewsList";
 export { NavigationLink } from "./NavigationLink";
