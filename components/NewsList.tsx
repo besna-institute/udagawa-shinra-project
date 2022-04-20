95a45f4dd8aea92ffd846e963ff9264bec4067ab
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-21 01:05:57 +0900

    Add key

--- components/NewsList.tsx
+++ components/NewsList.tsx
@@ -1,4 +1,5 @@
 import { List, ListItem, ListItemText, Divider } from "@mui/material";
+import { Fragment } from "react";
 
 interface Props {
   items: JSX.Element[];
@@ -6,12 +7,12 @@ interface Props {
 
 export const NewsList = ({ items }: Props) => {
   const newsItems = items.map((item, index) => (
-    <>
+    <Fragment key={index}>
       <ListItem key={index} disablePadding>
         <ListItemText primary={item} />
       </ListItem>
       <Divider key={`${index}-divider`} component="li" />
-    </>
+    </Fragment>
   ));
   return <List>{newsItems}</List>;
 };
