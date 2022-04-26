4d78649ebc1d33c0d66dcb0954c39564a0fbd399
Yudai Udagawa <nimius.rd@gmail.com>
2022-04-27 03:12:36 +0900

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,9 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
         />
+        <link rel="preload" href="shinra2022-fig1.png" as="image" />
+        <link rel="preload" href="shinra2022-fig2.png" as="image" />
+        <link rel="preload" href="shinra2022-fig3.png" as="image" />
       </Head>
       <body>
         <Main />
